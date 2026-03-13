728d51ca156f4a1fd86af67d0b57b0e1369951db
Hēsperus <hesprs@outlook.com>
2026-03-13 21:59:26 +0800

    fix dead import

--- src/Layout.vue
+++ src/Layout.vue
@@ -11,7 +11,6 @@ import { useData } from '@/composables/data';
 import { layoutInfoInjectionKey, registerWatchers } from '@/composables/layout';
 import { setupRouteChange } from './composables/route-change';
 import setupTransition from '@/composables/theme-toggle-transition';
-import { setupI18n } from './composables/i18n';
 
 const { frontmatter, isDark } = useData();
 setupTransition(isDark);
@@ -21,7 +20,6 @@ const heroImageSlotExists = computed(() => !!slots['home-hero-image']);
 
 registerWatchers();
 setupRouteChange();
-setupI18n();
 provide(layoutInfoInjectionKey, { heroImageSlotExists });
 </script>
 
