18f97ee4c5f4368b582c0d3ca8cd4cb8f8edab6c
Hēsperus <hesprs@outlook.com>
2026-07-15 22:38:24 +0800

    feat(UI): restore prev next links (#47)

--- src/composables/prev-next.ts
+++ src/composables/prev-next.ts
@@ -1,10 +1,24 @@
 import { computed } from 'vue';
+import { isActive } from '@/shared';
+import { getFlatSideBarLinks, getSidebar } from '@/support/sidebar';
 import useData from './data';
 
 export default function usePrevNext() {
-	const { theme, frontmatter } = useData();
+	const { page, theme, frontmatter } = useData();
 
 	return computed(() => {
+		const sidebar = getSidebar(theme.value.sidebar, page.value.relativePath);
+		const links = getFlatSideBarLinks(sidebar).filter(
+			(link, index, all) =>
+				index ===
+				all.findIndex(
+					(item) => item.link.replace(/[?#].*$/, '') === link.link.replace(/[?#].*$/, ''),
+				),
+		);
+		const index = links.findIndex((link) => isActive(page.value.relativePath, link.link));
+		const prev = links[index - 1];
+		const next = links[index + 1];
+
 		const hidePrev =
 			(theme.value.docFooter?.prev === false && !frontmatter.value.prev) ||
 			frontmatter.value.prev === false;
@@ -19,28 +33,32 @@ export default function usePrevNext() {
 				: {
 						link:
 							typeof frontmatter.value.next === 'object'
-								? frontmatter.value.next.link
-								: undefined,
+								? (frontmatter.value.next.link ?? next?.link)
+								: next?.link,
 						text:
 							typeof frontmatter.value.next === 'string'
 								? frontmatter.value.next
 								: typeof frontmatter.value.next === 'object'
-									? frontmatter.value.next.text
-									: undefined,
+									? (frontmatter.value.next.text ??
+										next?.docFooterText ??
+										next?.text)
+									: (next?.docFooterText ?? next?.text),
 					},
 			prev: hidePrev
 				? undefined
 				: {
 						link:
 							typeof frontmatter.value.prev === 'object'
-								? frontmatter.value.prev.link
-								: undefined,
+								? (frontmatter.value.prev.link ?? prev?.link)
+								: prev?.link,
 						text:
 							typeof frontmatter.value.prev === 'string'
 								? frontmatter.value.prev
 								: typeof frontmatter.value.prev === 'object'
-									? frontmatter.value.prev.text
-									: undefined,
+									? (frontmatter.value.prev.text ??
+										prev?.docFooterText ??
+										prev?.text)
+									: (prev?.docFooterText ?? prev?.text),
 					},
 		};
 	});
